ee122fa4be0a0bdfe8c299d356e245a3d8f75a45
BJ Choi <cbj1028@gmail.com>
2019-07-21 14:24:48 +0900

    20190721 - Carousel Update

--- application/views/footer.php
+++ application/views/footer.php
@@ -11,5 +11,23 @@
   <!-- Uncomment below i you want to use a preloader -->
   <!-- <div id="preloader"></div> -->
 
+  <!-- JavaScript Libraries -->
+  <script src="/public/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/public/lib/easing/easing.min.js"></script>
+  <script src="/public/lib/superfish/hoverIntent.js"></script>
+  <script src="/public/lib/superfish/superfish.min.js"></script>
+  <script src="/public/lib/wow/wow.min.js"></script>
+  <script src="/public/lib/waypoints/waypoints.min.js"></script>
+  <script src="/public/lib/counterup/counterup.min.js"></script>
+  <script src="/public/lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="/public/lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="/public/lib/lightbox/js/lightbox.min.js"></script>
+  <script src="/public/lib/touchSwipe/jquery.touchSwipe.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="/public/js/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="/public/js/main.js"></script>
+
 </body>
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -31,24 +31,6 @@
   <script src="/public/js/jquery-1.10.2.js" type="text/javascript"></script>
   <script src="/public/js/jquery-ui.custom.min.js" type="text/javascript"></script>
 
-  <!-- JavaScript Libraries -->
-  <script src="/public/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="/public/lib/easing/easing.min.js"></script>
-  <script src="/public/lib/superfish/hoverIntent.js"></script>
-  <script src="/public/lib/superfish/superfish.min.js"></script>
-  <script src="/public/lib/wow/wow.min.js"></script>
-  <script src="/public/lib/waypoints/waypoints.min.js"></script>
-  <script src="/public/lib/counterup/counterup.min.js"></script>
-  <script src="/public/lib/owlcarousel/owl.carousel.min.js"></script>
-  <script src="/public/lib/isotope/isotope.pkgd.min.js"></script>
-  <script src="/public/lib/lightbox/js/lightbox.min.js"></script>
-  <script src="/public/lib/touchSwipe/jquery.touchSwipe.min.js"></script>
-  <!-- Contact Form JavaScript File -->
-  <script src="/public/js/contactform.js"></script>
-
-  <!-- Template Main Javascript File -->
-  <script src="/public/js/main.js"></script>
-
 </head>
 
 <body>
