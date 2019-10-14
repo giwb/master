86ded000ccba56b17764e2dad3b67792822761b7
BJ Choi <cbj1028@gmail.com>
2019-10-14 14:44:52 +0900

    20191014 - CSS Mobile Update

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -28,7 +28,7 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
       <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?=base_url()?>admin">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center">
         <div class="sidebar-brand-text mx-3">경인웰빙 관리자</div>
       </a>
 
