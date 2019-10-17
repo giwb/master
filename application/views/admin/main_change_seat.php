f2b7a7defd84e05ec525f8b2ad38898ba9b44624
BJ Choi <cbj1028@gmail.com>
2019-10-17 13:52:39 +0900

    20191017 - Update2

--- application/views/admin/main_change_seat.php
+++ application/views/admin/main_change_seat.php
@@ -6,7 +6,7 @@
       <div id="content">
         <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
-          <h1 class="h3 mb-0 text-gray-800">진행중 산행 예약 보기</h1>
+          <h1 class="h3 mb-0 text-gray-800">진행중 산행 좌석 변경</h1>
         </div>
       </div>
     </div>
