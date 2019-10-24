081b0dfbdd01c251ae996f1848d66769634d6cde
BJ Choi <cbj1028@gmail.com>
2019-10-24 15:43:34 +0900

    20191024 - Updates8

--- application/views/header.php
+++ application/views/header.php
@@ -33,8 +33,10 @@
   <!-- Main Stylesheet File -->
   <link href="/public/css/style.css" rel="stylesheet">
 
-  <script src="/public/js/jquery-1.11.1.min.js" type="text/javascript"></script>
-  <script src="/public/js/jquery-ui.custom.min.js" type="text/javascript"></script>
+  <!--<script src="/public/js/jquery-1.11.1.min.js" type="text/javascript"></script>
+  <script src="/public/js/jquery-ui.custom.min.js" type="text/javascript"></script>-->
+<script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+<script src="http://code.jquery.com/ui/1.8.21/jquery-ui.min.js"></script>
 
   <!-- JavaScript Libraries -->
   <script src="/public/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
