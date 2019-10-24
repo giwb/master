e48cbf3a2512150e95ceece1aff747d23e099a3e
BJ Choi <cbj1028@gmail.com>
2019-10-24 13:34:04 +0900

    20191024 - Updates2

--- application/models/Club_model.php
+++ application/models/Club_model.php
@@ -55,7 +55,6 @@ class Club_model extends CI_Model
   {
     $this->db->select('*')
           ->from(DB_NOTICE)
-          ->where_in('status', array(1, 2))
           ->order_by('startdate', 'asc');
     return $this->db->get()->result_array();
   }
