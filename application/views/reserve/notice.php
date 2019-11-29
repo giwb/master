0ded0c29f4123814d3dce397aa610ee1546b0511
BJ Choi <cbj1028@gmail.com>
2019-11-29 16:34:38 +0900

    20191129 - Updates4

--- application/views/reserve/notice.php
+++ application/views/reserve/notice.php
@@ -6,25 +6,25 @@
             <div class="area-title"><h2><b>[<?=viewStatus($notice['status'])?>]</b> <?=$notice['subject']?></h2></div>
             <div class="area-btn"><a href="<?=base_url()?>reserve/<?=$view['idx']?>?n=<?=$notice['idx']?>"><button type="button" class="btn btn-primary">좌석보기</button></a></div>
           </div>
-          <?php if (!empty($notice['photo'])): ?><div class="sub-photo"><img src="<?=$notice['photo']?>"></div><?php endif; ?>
 
           <div class="sub-header">기획의도</div>
-          <div class="sub-content"><?=reset_html_escape($notice['plan'])?></div>
+          <div class="sub-content"><?=reset_html_escape($notice['plan'])?></div><p><br></p>
 
-          <div class="sub-header">핵심안내</div>
-          <div class="sub-content"><?=reset_html_escape($notice['point'])?></div>
-
-          <div class="sub-header">타임테이블</div>
-          <div class="sub-content"><?=reset_html_escape($notice['timetable'])?></div>
-
-          <div class="sub-header">산행안내</div>
-          <div class="sub-content"><?=reset_html_escape($notice['information'])?></div>
-
-          <div class="sub-header">산행코스안내</div>
-          <div class="sub-content"><?=reset_html_escape($notice['course'])?></div>
+          <div class="sub-header">여행개요</div>
+          <div class="sub-content"><?=reset_html_escape($notice['point'])?></div><p><br></p>
 
           <div class="sub-header">산행지소개</div>
-          <div class="sub-content"><?=reset_html_escape($notice['intro'])?></div>
+          <?php if (!empty($notice['photo'])): ?><div class="sub-photo"><img src="<?=$notice['photo']?>"></div><?php endif; ?>
+          <div class="sub-content"><?=reset_html_escape($notice['intro'])?></div><p><br></p>
+
+          <div class="sub-header">일정안내</div>
+          <div class="sub-content"><?=reset_html_escape($notice['timetable'])?></div><p><br></p>
+
+          <div class="sub-header">산행안내</div>
+          <div class="sub-content"><?=reset_html_escape($notice['information'])?></div><p><br></p>
+
+          <div class="sub-header">코스안내</div>
+          <div class="sub-content"><?=reset_html_escape($notice['course'])?></div><p><br></p>
 
           <?php if (!empty($notice['map'])): ?><div class="sub-photo"><img src="<?=$notice['map']?>"></div><?php endif; ?>
         </div>
