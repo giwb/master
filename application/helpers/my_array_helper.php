0f456a617b1a044e1b5d33f497f47f8308113255
BJ Choi <cbj1028@gmail.com>
2019-12-03 21:28:20 +0900

    20191203 - Updates29

--- application/helpers/my_array_helper.php
+++ application/helpers/my_array_helper.php
@@ -426,26 +426,22 @@ if (!function_exists('getReserve')) {
     }
     if ($result['idx'] == '') {
       // 붙어있는 좌석은 같은 성별로만
-      if ($userData['gender'] == '1') $message = "남성우선";
-      elseif ($userData['gender'] == '2') $message = "여성우선";
+      if ($userData['gender'] == '1') $message = '<span class="text-primary">남성우선</span>';
+      elseif ($userData['gender'] == '2') $message = '<span class="text-danger">여성우선</span>';
 
       if ($seat %2 == 1) {
         // 현재 좌석은 홀수
         $nextSeat = $seat + 1;
         if (!empty($checkGender[$bus][$nextSeat]) && $userData['gender'] != $checkGender[$bus][$nextSeat]) {
           $result['class'] = '';
-          $result['nickname'] = "<span class='text-danger'>";
-          $result['nickname'] .= !empty($message) ? $message : $checkGender[$bus][$nextSeat] == 'M' ? '남성우선' : '여성우선';
-          $result['nickname'] .= "</span>";
+          $result['nickname'] = !empty($message) ? $message : $checkGender[$bus][$nextSeat] == 'M' ? '<span class="text-primary">남성우선</span>' : '<span class="text-danger">여성우선</span>';
         }
       } elseif ($seat %2 == 0) {
         // 현재 좌석은 짝수
         $prevSeat = $seat - 1;
         if (!empty($checkGender[$bus][$prevSeat]) && $userData['gender'] != $checkGender[$bus][$prevSeat]) {
           $result['class'] = '';
-          $result['nickname'] = "<span class='text-danger'>";
-          $result['nickname'] .= !empty($message) ? $message : $checkGender[$bus][$prevSeat] == 'M' ? '남성우선' : '여성우선';
-          $result['nickname'] .= "</span>";
+          $result['nickname'] = !empty($message) ? $message : $checkGender[$bus][$prevSeat] == 'M' ? '<span class="text-primary">남성우선</span>' : '<span class="text-danger">여성우선</span>';
         }
       }
     }
