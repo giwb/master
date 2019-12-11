4542cb8999b238aa035fe50e63fc62e208a4dcfe
BJ Choi <cbj1028@gmail.com>
2019-12-12 03:49:51 +0900

    20191211 - Updates52

--- application/views/admin/log_user.php
+++ application/views/admin/log_user.php
@@ -10,7 +10,7 @@
         </form>
         <?php foreach ($listHistory as $value): ?>
           <div class="border-top pt-3 pb-3 pl-2 pr-2">
-            <strong><?=$value['header']?></strong> <?=$value['subject']?> - <a<?=!empty($value['userid']) ? ' target="_blank" href="' . base_url() . 'admin/member_view/' . $value['userid'] . '"' : ' href="javascript;"'?> class="text-secondary"><strong><?=$value['nickname']?>님</strong></a>
+            <strong><?=$value['header']?></strong> <?=$value['subject']?> - <a<?=!empty($value['userid']) ? ' target="_blank" href="' . base_url() . 'admin/member_view/' . $value['userid'] . '"' : ' href="javascript:;"'?> class="text-secondary"><strong><?=$value['nickname']?>님</strong></a>
             <div class="small"><?=calcStoryTime($value['regdate'])?> (<?=date('Y-m-d H:i:s', $value['regdate'])?>)</div>
           </div>
         <?php endforeach; ?>
