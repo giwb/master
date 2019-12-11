52e14542dafdcf0a96168702adbe7beb7a4c8a37
BJ Choi <cbj1028@gmail.com>
2019-12-12 02:37:11 +0900

    20191211 - Updates35

--- application/views/admin/log_user.php
+++ application/views/admin/log_user.php
@@ -10,7 +10,7 @@
         </form>
         <?php foreach ($listHistory as $value): ?>
           <div class="border-top pt-3 pb-3 pl-2 pr-2">
-            <strong><?=$value['header']?></strong> <?=$value['subject']?> - <a href="<?=base_url()?>admin/member_view/<?=$value['fkey']?>"><?=$value['nickname']?></a>
+            <strong><?=$value['header']?></strong> <?=$value['subject']?> - <a href="<?=base_url()?>admin/member_view/<?=$value['idx']?>"><?=$value['nickname']?></a>
           </div>
         <?php endforeach; ?>
         <div class="area-append">
