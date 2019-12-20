3869cc779948707c77ced2881fc34ab2f131020a
BJ Choi <cbj1028@gmail.com>
2019-12-20 10:04:29 +0900

    20191220 - Updates2

--- application/views/footer.php
+++ application/views/footer.php
@@ -18,7 +18,7 @@
 
   <?php if (empty($userData['idx'])): ?>
   <!-- Login Modal -->
-  <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+  <div class="modal" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
