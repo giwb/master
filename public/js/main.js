5de149d1f05be9303d7127f02269fd249e1aa8ea
BJ Choi <cbj1028@gmail.com>
2019-12-20 10:36:23 +0900

    20191220 - Updates5

--- public/js/main.js
+++ public/js/main.js
@@ -720,7 +720,7 @@
       $('.priority[data-bus=' + bus + '][data-id=' + priorityIdx + ']').addClass('active');
       $('html, body').animate( { scrollTop : $('#reserveForm').offset().top - 100 }, 1000 ); // 하단으로 스크롤
       $.viewReserveInfo(resIdx, bus, seat, priorityIdx);
-      setTimeout(function() { $.viewReserveInfo(priorityIdx, bus, prioritySeat, priorityIdx); }, 1000);
+      setTimeout(function() { $.viewReserveInfo(priorityIdx, bus, prioritySeat, priorityIdx); }, 500);
     }
   }).on('click', '.btn-reserve-confirm', function() {
     // 좌석 예약
