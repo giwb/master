41adfad4f4427e55d236e1cf8bb969423cc90502
BJ Choi <cbj1028@gmail.com>
2019-12-23 16:52:15 +0900

    20191223 - Updates15

--- application/views/header.php
+++ application/views/header.php
@@ -110,8 +110,8 @@
           <li><p>&nbsp;</p></li>
           <?php endif; ?>
           <li class="row">
-            <div class="pl-3"><a target="_blank" href="https://pf.kakao.com/_BxaPRxb/friend"><img src="<?=base_url()?>public/images/icon_kakao_channel_sp.png"></a></div>
-            <div class="pl-3"><a target="_blank" href="https://facebook.com/giwb.kr"><img height="30" src="<?=base_url()?>public/images/icon_facebook.png"></a></div>
+            <div class="pl-3"><a target="_blank" title="카카오채널 링크" href="https://pf.kakao.com/_BxaPRxb"><img src="<?=base_url()?>public/images/icon_kakaotalk.png"></a></div>
+            <div class="pl-3"><a target="_blank" title="페이스북 링크" href="https://facebook.com/giwb.kr"><img src="<?=base_url()?>public/images/icon_facebook.png"></a></div>            
           </li>
           <li><a href="<?=base_url()?>club/about/<?=$view['idx']?>"> 산악회 소개</a></li>
           <li><a href="<?=base_url()?>club/guide/<?=$view['idx']?>"> 등산 안내인 소개</a></li>
@@ -173,8 +173,8 @@
           ・방문수 : <?=number_format($view['cntVisitor']['cnt'])?>회 / 오늘 <?=number_format($view['cntVisitorToday']['cnt'])?>명<br>
           </div>
           <div class="text-center border-top mt-4 pt-3">
-            <a target="_blank" href="https://pf.kakao.com/_BxaPRxb/friend"><img src="<?=base_url()?>public/images/icon_kakao_channel.png"></a>
-            <a target="_blank" href="https://facebook.com/giwb.kr"><img height="45" src="<?=base_url()?>public/images/btn_facebook.png"></a>
+            <a target="_blank" title="카카오채널 링크" href="https://pf.kakao.com/_BxaPRxb"><img src="<?=base_url()?>public/images/icon_kakaotalk.png"></a>
+            <a target="_blank" title="페이스북 링크" href="https://facebook.com/giwb.kr"><img src="<?=base_url()?>public/images/icon_facebook.png"></a>
           </div>
         </div>
       </div>
