ee2007068b07ed37a3528f56ff56372d65b8b4b1
BJ Choi <cbj1028@gmail.com>
2019-12-28 05:03:56 +0900

    20191227 - Updates25

--- application/views/shop/list.php
+++ application/views/shop/list.php
@@ -3,7 +3,7 @@
   <div class="col-sm-1"><img src="<?=$value['item_photo']?>" class="w-100"></div>
   <div class="col-sm-11">
     ・등록 : <?=date('Y-m-d H:i:s', $value['created_at'])?><br>
-    ・분류 : <?php foreach ($value['item_category_name'] as $key => $cname): ?><?=$key != 0 ? ' > ' : ''?><?=$cname?><? endforeach; ?><br>
+    ・분류 : <?php foreach ($value['item_category_name'] as $key => $cname): ?><?=$key != 0 ? ' &gt; ' : ''?><?=$cname?><? endforeach; ?><br>
     ・품명 : <?=$value['item_name']?><br>
     ・가격<?php if (count($value['item_option_cost']) == 1 && empty($value['item_option'][0])): ?> : <?=number_format($value['item_option_cost'][0])?>원<?php else: ?><br><?php foreach ($value['item_option_cost'] as $key => $cost): ?><div class="pl-3"><?=!empty($value['item_option'][$key]) ? $value['item_option'][$key] . ' - ' : ''?><?=number_format($cost)?>원</div><?php endforeach; endif; ?>
     <br>
