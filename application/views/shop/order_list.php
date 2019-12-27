cfeb0f7874420c9823d90ac34ba0f9cd4144bb52
BJ Choi <cbj1028@gmail.com>
2019-12-27 18:25:28 +0900

    20191227 - Updates19

--- application/views/shop/order_list.php
+++ application/views/shop/order_list.php
@@ -1,6 +1,6 @@
 <?php foreach ($listOrder as $value): ?>
-<div class="border">
-  <div class="bg-secondary text-white p-2"><?=!empty($value['status']) && $value['status'] == RESERVE_PAY ? '<strong class="text-primary">[입금완료]</strong>' : '<strong class="text-dark">[입금대기]</strong>'?> <?=$value['nickname']?>님 - <?=date('Y-m-d', $value['created_at'])?> (<?=calcWeek(date('Y-m-d', $value['created_at']))?>) <?=date('H:i', $value['created_at'])?></div>
+<div class="border bg-white">
+  <div class="bg-light p-2"><?=!empty($value['status']) && $value['status'] == RESERVE_PAY ? '<strong class="text-primary">[입금완료]</strong>' : '<strong class="text-secondary">[입금대기]</strong>'?> <a href="<?=base_url()?>admin/member_view/<?=$value['created_by']?>"><?=$value['nickname']?>님</a> - <?=date('Y-m-d', $value['created_at'])?> (<?=calcWeek(date('Y-m-d', $value['created_at']))?>) <?=date('H:i', $value['created_at'])?></div>
   <div class="p-3">
     ・구매금액 : <?=number_format($value['totalCost'])?>원 / 사용한 포인트 : <?=number_format($value['point'])?>원<br>
     ・인수산행 : <?php if (!empty($value['startdate'])): ?><?=$value['startdate']?> (<?=calcWeek($value['startdate'])?>) <?=$value['mname']?><? else: ?>미지정<?php endif; ?>
