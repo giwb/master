be2ddd2ac77345e43f2e63672c83843cf72d288c
BJ Choi <cbj1028@gmail.com>
2019-12-29 12:00:10 +0900

    20191229 - Updates1

--- application/views/admin/log_user_append.php
+++ application/views/admin/log_user_append.php
@@ -1,6 +1,6 @@
 <?php foreach ($listHistory as $value): ?>
 <div class="border-top pt-3 pb-3 pl-2 pr-2">
-  <strong><?=$value['header']?></strong> <?=$value['subject']?> - <a target="_blank" href="<?=base_url()?>admin/member_view/<?=$value['userid']?>"><strong><?=$value['nickname']?>님</strong></a>
+  <strong><?=$value['header']?></strong> <?=$value['subject']?> - <a target="_blank" href="<?=!empty($value['userid']) ? base_url() . 'admin/member_view/' . $value['userid'] : 'javascript:;'?>"><strong><?=$value['nickname']?>님</strong></a>
   <div class="small"><?=calcStoryTime($value['regdate'])?> (<?=date('Y-m-d H:i:s', $value['regdate'])?>)</div>
 </div>
 <?php endforeach; ?>
