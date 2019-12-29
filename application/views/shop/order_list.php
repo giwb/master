6f12a93193a933e87a11a6aac6d8e7dc90719329
BJ Choi <cbj1028@gmail.com>
2019-12-29 12:01:07 +0900

    20191229 - Updates2

--- application/views/shop/order_list.php
+++ application/views/shop/order_list.php
@@ -1,5 +1,5 @@
 <?php foreach ($listOrder as $value): ?>
-<div class="border bg-white">
+<div class="border bg-white mb-4">
   <div class="bg-light p-2"><?=!empty($value['status']) && $value['status'] == RESERVE_PAY ? '<strong class="text-primary">[입금완료]</strong>' : '<strong class="text-secondary">[입금대기]</strong>'?> <a href="<?=base_url()?>admin/member_view/<?=$value['created_by']?>"><?=$value['nickname']?>님</a> - <?=date('Y-m-d', $value['created_at'])?> (<?=calcWeek(date('Y-m-d', $value['created_at']))?>) <?=date('H:i', $value['created_at'])?></div>
   <div class="p-3">
     ・구매금액 : <?=number_format($value['totalCost'])?>원 / 사용한 포인트 : <?=number_format($value['point'])?>원<br>
