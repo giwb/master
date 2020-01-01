8a1dd94216798c1ff723c161818e3e2660c88b3f
BJ Choi <cbj1028@gmail.com>
2020-01-01 15:07:17 +0900

    20200101 - Updates11

--- application/views/club/shop_list.php
+++ application/views/club/shop_list.php
@@ -1,7 +1,7 @@
 <?php foreach ($listItem as $key => $value): ?>
-  <?php if ($key == 0): ?><div class="row"><?php elseif ($key%2 == 0): ?></div><div class="row"><?php endif; ?>
-  <div class="col-sm-6 text-center mb-4">
-    <div class="shop-item" data-idx="<?=$value['idx']?>">
+  <?php if ($key == 0): ?><div class="row d-flex justify-content-between"><?php elseif ($key%3 == 0): ?></div><div class="row d-flex justify-content-between"><?php endif; ?>
+  <div class="col-sm-4 text-center mb-4 pl-2 pr-2">
+    <div class="shop-item p-3 h-100" data-idx="<?=$value['idx']?>">
       <div class="item-photo"><img src="<?=$value['item_photo']?>"></div>
       <div class="item-category"><?php if (!empty($value['item_category_name'])): foreach ($value['item_category_name'] as $cnt => $cname): ?><?=$cnt != 0 ? ' &gt; ' : ''?><?=$cname?><?php endforeach; endif; ?></div>
       <div class="pt-1 pb-1"><?=$value['item_name']?></div>
