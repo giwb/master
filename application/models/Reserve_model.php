dbdcb67ccd089d2db7ce6a4e9e0db9fd31cbf724
BJ Choi <cbj1028@gmail.com>
2020-01-25 14:44:40 +0900

    20200125 - TripKorea New60

--- application/models/Reserve_model.php
+++ application/models/Reserve_model.php
@@ -135,7 +135,7 @@ class Reserve_model extends CI_Model
           ->join(DB_NOTICE . ' b', 'a.fkey=b.idx', 'left')
           ->where('a.club_idx', $clubIdx)
           ->where('a.userid', $userId)
-          ->where('a.action', LOG_CANCEL)
+          ->where_in('a.action', array(LOG_CANCEL, LOG_ADMIN_CANCEL))
           ->where('b.visible', VISIBLE_ABLE)
           ->order_by('a.idx', 'desc');
 
