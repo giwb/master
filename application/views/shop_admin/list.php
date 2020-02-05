9ac5198d82337efa37c9a6d251ff7491083a957e
BJ Choi <cbj1028@gmail.com>
2020-02-05 14:17:31 +0900

    20200204 - Old Admin Update13

--- application/views/shop_admin/list.php
+++ application/views/shop_admin/list.php
@@ -1,7 +1,7 @@
 <?php foreach ($listItem as $value): ?>
 <div class="row align-items-center mb-3 item-list" data-idx="<?=$value['idx']?>">
-  <div class="col-sm-1"><img src="<?=$value['item_photo']?>" class="w-100"></div>
-  <div class="col-sm-11">
+  <div class="col-sm-2"><img src="<?=$value['item_photo']?>" class="w-100"></div>
+  <div class="col-sm-10">
     ・등록 : <?=date('Y-m-d H:i:s', $value['created_at'])?><br>
     ・분류 : <?php foreach ($value['item_category_name'] as $key => $cname): ?><?=$key != 0 ? ' &gt; ' : ''?><?=$cname?><?php endforeach; ?><br>
     ・품명 : <?=$value['item_name']?><br>
