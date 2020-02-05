982e660c02905a356ab26c0d7cdfc1bf94c957f8
BJ Choi <cbj1028@gmail.com>
2020-02-05 14:20:36 +0900

    20200204 - Old Admin Update14

--- application/views/admin/main_view_progress.php
+++ application/views/admin/main_view_progress.php
@@ -2,12 +2,12 @@
 
         <div id="content" class="mb-5">
           <div class="row align-items-center border-bottom text-center mb-4 pt-3 pb-3">
-            <div class="col"><a href="<?=BASE_URL?>/admin/main_entry/<?=$view['idx']?>"><button type="button" class="btn btn-sm btn-default w-100">수정</button></a></div>
-            <div class="col"><a href="<?=BASE_URL?>/admin/main_notice/<?=$view['idx']?>"><button type="button" class="btn btn-sm btn-default w-100">공지</button></a></div>
-            <div class="col"><a href="<?=BASE_URL?>/admin/main_view_progress/<?=$view['idx']?>"><button type="button" class="btn btn-sm btn-secondary w-100">예약</button></a></div>
-            <div class="col"><a href="<?=BASE_URL?>/admin/main_view_boarding/<?=$view['idx']?>"><button type="button" class="btn btn-sm btn-default w-100">승차</button></a></div>
-            <div class="col"><a href="<?=BASE_URL?>/admin/main_view_sms/<?=$view['idx']?>"><button type="button" class="btn btn-sm btn-default w-100">문자</button></a></div>
-            <div class="col"><a href="<?=BASE_URL?>/admin/main_view_adjust/<?=$view['idx']?>"><button type="button" class="btn btn-sm btn-default w-100">정산</button></a></div>
+            <div class="col pt-0 pr-1 pb-0 pl-1"><a href="<?=BASE_URL?>/admin/main_entry/<?=$view['idx']?>"><button type="button" class="btn btn-sm btn-default w-100">수정</button></a></div>
+            <div class="col pt-0 pr-1 pb-0 pl-1"><a href="<?=BASE_URL?>/admin/main_notice/<?=$view['idx']?>"><button type="button" class="btn btn-sm btn-default w-100">공지</button></a></div>
+            <div class="col pt-0 pr-1 pb-0 pl-1"><a href="<?=BASE_URL?>/admin/main_view_progress/<?=$view['idx']?>"><button type="button" class="btn btn-sm btn-secondary w-100">예약</button></a></div>
+            <div class="col pt-0 pr-1 pb-0 pl-1"><a href="<?=BASE_URL?>/admin/main_view_boarding/<?=$view['idx']?>"><button type="button" class="btn btn-sm btn-default w-100">승차</button></a></div>
+            <div class="col pt-0 pr-1 pb-0 pl-1"><a href="<?=BASE_URL?>/admin/main_view_sms/<?=$view['idx']?>"><button type="button" class="btn btn-sm btn-default w-100">문자</button></a></div>
+            <div class="col pt-0 pr-1 pb-0 pl-1"><a href="<?=BASE_URL?>/admin/main_view_adjust/<?=$view['idx']?>"><button type="button" class="btn btn-sm btn-default w-100">정산</button></a></div>
           </div>
           <div class="sub-contents">
             <h2><b><?=viewStatus($view['status'])?></b> <?=$view['subject']?></h2>
