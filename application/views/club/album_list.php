e1435702c92d01e8076478b7bf4c2065177374fb
BJ Choi <cbj1028@gmail.com>
2020-02-13 04:34:06 +0900

    20200212 - NewUpdates10

--- application/views/club/album_list.php
+++ application/views/club/album_list.php
@@ -1,5 +1,5 @@
-<?php foreach ($listAlbumMain as $key => $value): ?>
-  <?php if ($key == 0): ?><div class="row"><?php elseif ($key%3 == 0): ?></div><div class="row"><?php endif; ?>
-  <div class="col-sm-4 text-center mb-4 album-item"><a href="javascript:;" class="btn-album" data-idx="<?=$value['idx']?>"><img class="album-photo border mb-2" src="<?=$value['photo']?>"></a><br><?=$value['subject']?><br><span class="small"><?=$value['nickname']?>님 | <?=calcStoryTime($value['created_at'])?><?=$value['created_by'] == $userIdx || !empty($adminCheck) ? ' | <a href="' . BASE_URL . '/album/entry/?n=' . $value['idx'] .'">수정</a>' : ''?></span></div>
-<?php endforeach; ?>
-<?php if (!empty($listAlbumMain)): ?></div><?php endif; ?>
+<div class="row">
+  <?php foreach ($listAlbumMain as $key => $value): ?>
+  <div class="col-6 col-sm-4 mb-4 p-1 text-center album-item"><a href="javascript:;" class="btn-album" data-idx="<?=$value['idx']?>"><img class="album-photo border mb-2" src="<?=$value['photo']?>"></a><br><?=$value['subject']?><br><span class="small"><?=$value['nickname']?>님 | <?=calcStoryTime($value['created_at'])?><?=$value['created_by'] == $userIdx || !empty($adminCheck) ? ' | <a href="' . BASE_URL . '/album/entry/?n=' . $value['idx'] .'">수정</a>' : ''?></span></div>
+  <?php endforeach; ?>
+</div>
