3ca723fcb1ede77bc92a1587a82ebf17afe07830
BJ Choi <cbj1028@gmail.com>
2020-05-07 17:15:18 +0900

    20200506 - Updates7

--- application/views/admin/main_list_progress.php
+++ application/views/admin/main_list_progress.php
@@ -6,13 +6,22 @@
             등록된 산행이 없습니다.
           </div>
           <?php else: foreach ($list as $value): ?>
-          <div class="border-bottom mt-1 pt-2 pb-2">
-            <b><?=viewStatus($value['status'], $value['visible'])?></b> <a href="<?=BASE_URL?>/admin/main_view_progress/<?=$value['idx']?>"><?=$value['subject']?></a><br>
-            <div class="small">
-              <?php if (!empty($value['sido'])): ?>
-              <?php foreach ($value['sido'] as $key => $sido): ?><?=$sido?> <?=!empty($value['gugun'][$key]) ? $value['gugun'][$key] : ''?>, <?php endforeach; ?>
+          <div class="row align-items-center border-bottom mt-1 pt-2 pb-2">
+            <div class="col-9 col-sm-10">
+              <b><?=viewStatus($value['status'], $value['visible'])?></b> <a href="<?=BASE_URL?>/admin/main_view_progress/<?=$value['idx']?>"><?=$value['subject']?></a><br>
+              <div class="small">
+                <?php if (!empty($value['sido'])): ?>
+                <?php foreach ($value['sido'] as $key => $sido): ?><?=$sido?> <?=!empty($value['gugun'][$key]) ? $value['gugun'][$key] : ''?>, <?php endforeach; ?>
+                <?php endif; ?>
+                <?=$value['startdate']?> (<?=calcWeek($value['startdate'])?>) <?=$value['starttime']?> / <?=number_format($value['cost_total'] == 0 ? $value['cost'] : $value['cost_total'])?>원 / 예약인원 <?=cntRes($value['idx'])?>명
+              </div>
+            </div>
+            <div class="col-3 col-sm-2 text-right">
+              <?php if ($value['visible'] == VISIBLE_ABLE): ?>
+              <button type="button" class="btn btn-sm btn-secondary btn-change-visible" data-idx="<?=$value['idx']?>" data-visible="<?=VISIBLE_NONE?>">숨김</button>
+              <?php else: ?>
+              <button type="button" class="btn btn-sm btn-default btn-change-visible" data-idx="<?=$value['idx']?>" data-visible="<?=VISIBLE_ABLE?>">공개</button>
               <?php endif; ?>
-              <?=$value['startdate']?> (<?=calcWeek($value['startdate'])?>) <?=$value['starttime']?> / <?=number_format($value['cost_total'] == 0 ? $value['cost'] : $value['cost_total'])?>원 / 예약인원 <?=cntRes($value['idx'])?>명
             </div>
           </div>
           <?php endforeach; endif; ?>
