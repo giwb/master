05c60e3eb8349ce9eebe2ca908a068623289da21
BJ Choi <cbj1028@gmail.com>
2020-05-16 13:52:16 +0900

    20200516 - Updates1

--- application/views/story/index.php
+++ application/views/story/index.php
@@ -15,6 +15,13 @@
       $size = getImageSize(PHOTO_PATH . $value['filename']);
       $value['file_width'] = $size[0];
       $value['file_height'] = $size[1];
+      $value['file_thumb'] = PHOTO_URL . 'thumb_' . $value['filename'];
+      $value['filename'] = PHOTO_URL . $value['filename'];
+    } else {
+      $value['file_width'] = 0;
+      $value['file_height'] = 0;
+      $value['file_thumb'] = '';
+      $value['filename'] = '';
     }
 ?>
           <article id="post-<?=$value['idx']?>">
@@ -24,7 +31,7 @@
                 <a href="<?=BASE_URL?>/story/view/<?=$value['idx']?>" class="story-date"><?=calcStoryTime($value['created_at'])?><?=!empty($value['updated_at']) ? ' 작성, ' . calcStoryTime($value['updated_at']) . ' 수정' : ''?></a><?=!empty($userData['idx']) && ($userData['idx'] == $value['created_by'] || $userData['admin'] == 1) ? ' <a href="' . BASE_URL . '/story/edit/' . $value['idx'] . '">[수정]</a> <a href="javascript:;" class="btn-post-delete-modal" data-idx="' . $value['idx'] . '" data-action="delete">[삭제]</a>' : ''?>
               </div>
               <div class="story-content">
-                <?php if (!empty($value['filename'])): ?><img class="story-photo" src="<?=PHOTO_URL?>thumb_<?=$value['filename']?>" data-filename="<?=PHOTO_URL?><?=$value['filename']?>" data-width="<?=$value['file_width']?>" data-height="<?=$value['file_height']?>"><br><?php endif; ?>
+                <?php if (!empty($value['file_thumb'])): ?><img class="story-photo" src="<?=$value['file_thumb']?>" data-filename="<?=$value['filename']?>" data-width="<?=$value['file_width']?>" data-height="<?=$value['file_height']?>"><br><?php endif; ?>
                 <?=nl2br(strip_tags($value['content']))?>
               </div>
               <div class="story-reaction">
