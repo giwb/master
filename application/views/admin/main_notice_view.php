4a88569b96d9e851e92236030d206328ff8df68a
BJ Choi <cbj1028@gmail.com>
2020-06-04 01:11:15 +0900

    20200604 - Updates9

--- application/views/admin/main_notice_view.php
+++ application/views/admin/main_notice_view.php
@@ -5,30 +5,23 @@
   <title><?=$view['title']?> - 공지 보기</title>
   <link href="/public/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <style type="text/css">
-    article { width: 648px; }
-    .notice-title { font-size: 24px; font-family: "Malgun Gothic"; font-weight: bold; margin: 0px; padding: 0px; float: left; }
-    .notice-button { float: right; }
-    .sub-notice-header { font-weight: bold; background-color: #d9d9d9; border: 1px solid #999; border-width: 1px 0; margin: 0px; padding: 7px 15px; }
-    .sub-content { font-size: 14px; text-align: justify; overflow: hidden; }
     .sub-content img { max-width: 100%; cursor: pointer; }
-    .btn-default { color: #fff; background-color: #FF6C00; }
-    .btn-default:hover { color: #fff; background-color: #FF842A; }
   </style>
 </head>
 <body>
 
-<article>
-  <h2 class="notice-title"><?=$notice['subject']?></h2></td>
-  <div class="notice-button text-right pb-3">
-    <?=!empty($notice['weather']) ? '<a target="_blank" href="' . $notice['weather'] . '"><button type="button" class="btn btn-sm btn-primary mr-2">날씨</button></a>' : ''?>
-    <a target="_blank" href="<?=BASE_URL?>/reserve/list/<?=$notice['idx']?>"><button type="button" class="btn btn-sm btn-default btn-notice">좌석</button></a>
-  </div>
+<div style="clear:both;">
+  <h2 style="float: left; font-size: 24px; font-family: 'Malgun Gothic'; font-weight: bold; margin: 0px; padding: 0px;"><?=$notice['subject']?></h2>
+  <a target="_blank" href="<?=BASE_URL?>/reserve/list/<?=$notice['idx']?>"><button type="button" style="float: right; border-radius: 0.2rem; font-family: inherit; font-size: 0.875rem; line-height: 1.5; overflow: visible; color: rgb(255, 255, 255); text-align: center; vertical-align: middle; user-select: none; background-color: rgb(0, 123, 255); border-width: 1px; border-style: solid; border-color: rgb(0, 123, 255); padding: 0.25rem 0.5rem; transition: color 0.15s ease-in-out 0s, background-color 0.15s ease-in-out 0s, border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s; cursor: pointer;">좌석</button></a>
+  <?=!empty($notice['weather']) ? '<a target="_blank" href="' . $notice['weather'] . '"><button type="button" style="float: right; border-radius: 0.2rem; font-family: inherit; font-size: 0.875rem; line-height: 1.5; overflow: visible; color: rgb(255, 255, 255); text-align: center; vertical-align: middle; user-select: none; background-color: rgb(255, 108, 0); border-width: 1px; border-style: solid; border-color: transparent; margin: 0 0.5rem 0 0; padding: 0.25rem 0.5rem; transition: color 0.15s ease-in-out 0s, background-color 0.15s ease-in-out 0s, border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s; cursor: pointer;">날씨</button></a>' : ''?>
+</div>
 
+<div style="clear:both; padding-top: 20px;">
   <?php foreach ($listNoticeDetail as $value): ?>
-  <div class="sub-notice-header"><?=$value['title']?></div>
-  <div class="sub-content"><?=$value['content']?></div><br>
+  <div style="font-weight: bold; background-color: #d9d9d9; border: 1px solid #999; border-width: 1px 0; margin: 0px; padding: 7px 15px;"><?=$value['title']?></div>
+  <div class="sub-content" style="font-size: 14px; text-align: justify; overflow: hidden;"><?=$value['content']?></div><br>
   <?php endforeach; ?>
-</article>
+</div>
 
 </body>
 </html>
