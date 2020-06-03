3b0ba5b303f57110fbefe6cbe97c0fc186ba5cd7
BJ Choi <cbj1028@gmail.com>
2020-06-04 01:18:06 +0900

    20200604 - Updates12

--- application/views/admin/main_notice_view.php
+++ application/views/admin/main_notice_view.php
@@ -11,9 +11,6 @@
 <body>
 
 <div style="clear:both;">
-  <h2><div style="float: left; font-size: 24px; font-family: 'Malgun Gothic'; font-weight: bold; margin: 0px; padding: 0px;"><?=$notice['subject']?></div></h2>
-</div>
-<div style="clear:both; padding-top: 5px;">
   <a target="_blank" href="<?=BASE_URL?>/reserve/list/<?=$notice['idx']?>"><button type="button" style="float: right; border-radius: 0.2rem; font-family: inherit; font-size: 0.875rem; line-height: 1.5; overflow: visible; color: rgb(255, 255, 255); text-align: center; vertical-align: middle; user-select: none; background-color: rgb(0, 123, 255); border-width: 1px; border-style: solid; border-color: rgb(0, 123, 255); padding: 0.25rem 0.5rem; transition: color 0.15s ease-in-out 0s, background-color 0.15s ease-in-out 0s, border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s; cursor: pointer;">좌석</button></a>
   <?=!empty($notice['weather']) ? '<a target="_blank" href="' . $notice['weather'] . '"><button type="button" style="float: right; border-radius: 0.2rem; font-family: inherit; font-size: 0.875rem; line-height: 1.5; overflow: visible; color: rgb(255, 255, 255); text-align: center; vertical-align: middle; user-select: none; background-color: rgb(255, 108, 0); border-width: 1px; border-style: solid; border-color: transparent; margin: 0 0.5rem 0 0; padding: 0.25rem 0.5rem; transition: color 0.15s ease-in-out 0s, background-color 0.15s ease-in-out 0s, border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s; cursor: pointer;">날씨</button></a>' : ''?>
 </div>
