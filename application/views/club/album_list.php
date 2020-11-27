d53d4d6a8d0e103692644017885e8f4eb0afbf55
unknown <cbj1028@gmail.com>
2020-11-27 18:48:10 +0900

    20201126 - 디자인 추가17

--- application/views/club/album_list.php
+++ application/views/club/album_list.php
@@ -1,5 +1,5 @@
 <div class="row">
   <?php foreach ($listAlbumMain as $key => $value): ?>
-  <div class="col-6 col-sm-3 mb-4 p-3 text-center album-item"><a href="javascript:;" class="btn-album" data-idx="<?=$value['idx']?>"><img class="album-photo border mb-2" src="<?=$value['photo']?>"></a><br><?=$value['subject']?><br><span class="small"><?=$value['nickname']?>님 | <?=calcStoryTime($value['created_at'])?><?=$value['created_by'] == $userIdx || !empty($adminCheck) ? ' | <a href="' . BASE_URL . '/album/entry/?n=' . $value['idx'] .'">수정</a>' : ''?></span></div>
+  <div class="col-6 col-sm-3 p-2 text-center album-item"><a href="javascript:;" class="btn-album" data-idx="<?=$value['idx']?>"><img class="album-photo border mb-2" src="<?=$value['photo']?>"></a><br><?=$value['subject']?><br><span class="small"><?=$value['nickname']?>님 | <?=calcStoryTime($value['created_at'])?><?=$value['created_by'] == $userIdx || !empty($adminCheck) ? ' | <a href="' . BASE_URL . '/album/entry/?n=' . $value['idx'] .'">수정</a>' : ''?></span></div>
   <?php endforeach; ?>
 </div>
