b093b0f8b8053dbc456f0973b29cb819294e06bc
unknown <cbj1028@gmail.com>
2020-12-01 14:53:05 +0900

    20201130 - Giwb Update2

--- application/views/admin/footer_2.php
+++ application/views/admin/footer_2.php
@@ -11,7 +11,7 @@
           <?php if (!empty($value['photo']) && file_exists(PHOTO_PATH . 'thumb_' . $value['photo'])): ?>
           <img class="notice-photo" align="left" src="<?=PHOTO_URL . 'thumb_' . $value['photo']?>">
           <?php else: ?>
-          <img class="notice-photo" align="left" src="/public/images/nophoto.gif">
+          <img class="notice-photo" align="left" src="/public/images/nophoto.png">
           <?php endif; ?>
           <?=viewStatus($value['status'])?> <strong><?=$value['subject']?></strong><br><small><?=$value['startdate']?> (<?=calcWeek($value['startdate'])?>) <?=$value['starttime']?> / <?=number_format($value['cost_total'] == 0 ? $value['cost'] : $value['cost_total'])?>원 / <?=cntRes($value['idx'])?>명</small>
           <?php endforeach; ?>
