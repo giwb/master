a46184456a928f204a7b9bee8251c0640965be0b
unknown <cbj1028@gmail.com>
2021-01-07 13:36:14 +0900

    20210107 - TripKorea Domain SSL

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://tripkorea.net/';
+$config['base_url'] = 'https://tripkorea.net/';
 
 /*
 |--------------------------------------------------------------------------
