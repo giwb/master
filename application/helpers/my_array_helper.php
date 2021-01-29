86c26f7f19fb57efd0b90fef49fe092f060d7487
unknown <cbj1028@gmail.com>
2021-01-29 17:14:21 +0900

    20210129 - Giwb Update3

--- application/helpers/my_array_helper.php
+++ application/helpers/my_array_helper.php
@@ -420,18 +420,18 @@ if (!function_exists('calcDistance')) {
 // 승차위치
 if (!function_exists('arrLocation')) {
   function arrLocation($club_geton=NULL, $starttime=NULL, $location=NULL, $stitle=NULL, $start=NULL) {
-    $result = array(array('no' => '0', 'time' => '', 'title' => '', 'stitle' => ''));
+    //$result = array(array('no' => '0', 'time' => '', 'title' => '', 'stitle' => ''));
     if (!empty($club_geton)) {
       if (!is_null($starttime)) {
         $starttime = strtotime($starttime);
       }
       $arrGeton = unserialize($club_geton);
-
+/*
       foreach ($arrGeton as $key => $value) {
         $arr = array('no' => $key+1, 'time' => !is_null($starttime) ? date('H:i', $starttime + (60 * ($key*5))) : '', 'title' => $value, 'stitle' => substr($value, 0, 6));
         array_push($result, $arr);
       }
-/*
+*/
       $result = array(
         array('no' => '0', 'time' => '', 'title' => '', 'stitle' => ''),
         array('no' => '1', 'time' => !is_null($starttime) ? date('H:i', $starttime) : '', 'title' => '계산역 4번출구', 'stitle' => '계산'),
@@ -443,6 +443,7 @@ if (!function_exists('arrLocation')) {
         array('no' => '7', 'time' => !is_null($starttime) ? date('H:i', $starttime + (60 * 25)) : '', 'title' => '복사골 문화센터', 'stitle' => '복사'),
         array('no' => '8', 'time' => !is_null($starttime) ? date('H:i', $starttime + (60 * 30)) : '', 'title' => '송내남부 맥도날드', 'stitle' => '송내'),
       );
+      /*
       if (is_null($start)) { // 원종동은 정규 노선이 아니기 때문에 문자 발송시 참조만 한다
         $arr = array('no' => '9', 'time' => !is_null($starttime) ? date('H:i', $starttime + (60 * -20)) : '', 'title' => '원종동', 'stitle' => '원종');
         array_push($result, $arr);
