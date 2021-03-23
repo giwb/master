1e71ed8195c2f985edef262625e4548e9215230e
unknown <cbj1028@gmail.com>
2021-03-24 01:00:39 +0900

    20210322 - New Tripkorea64

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -520,10 +520,10 @@ class Login extends MY_Controller
           )
         );
         $header = array(
-          "'Content-Type': 'application/json; charset=utf-8'",
-          "'x-ncp-apigw-timestamp': '" . $timestamp . "'",
-          "'x-ncp-iam-access-key': '" . $access_key . "'",
-          "'x-ncp-apigw-signature-v2': '" . base64_encode(hash_hmac('sha256', $string, $secret_key)) . "'"
+          'Content-Type: application/json; charset=utf-8',
+          'x-ncp-apigw-timestamp: ' . $timestamp,
+          'x-ncp-iam-access-key: ' . $access_key,
+          'x-ncp-apigw-signature-v2: ' . base64_encode(hash_hmac('sha256', $string, $secret_key))
         );
         $message = '[경인웰빙투어] 인증번호는 ' . $auth_code . ' 입니다.';
 
