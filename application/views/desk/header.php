9ac60f05a9fa32630bb50df3e339f17d21277a43
unknown <cbj1028@gmail.com>
2021-03-25 09:44:56 +0900

    20210325 - New Tripkorea2

--- application/views/desk/header.php
+++ application/views/desk/header.php
@@ -3,10 +3,11 @@
 <html lang="ko">
 <head>
     <meta charset="utf-8">
+    <meta name="robots" content="noindex,nofollow">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <link rel="icon" type="image/png" href="/public/images/tripkorea/favicon.png">
+    <link rel="shortcut icon" type="image/png" href="/public/images/tripkorea/favicon.png">
 
     <title>한국여행 데스크</title>
 
