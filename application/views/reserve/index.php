ee457177b9996849770c928e7fb467958c31b838
unknown <cbj1028@gmail.com>
2021-03-30 10:16:21 +0900

    20210330 - New Tripkorea6

--- application/views/reserve/index.php
+++ application/views/reserve/index.php
@@ -62,9 +62,9 @@
             <hr class="text-default mt-2">
 
             <div class="header-menu d-block-inline d-sm-none pt-2">
-              <div class="header-menu-item active"><a href="<?=BASE_URL?>/list/<?=$notice['idx']?>">좌석</a></div>
-              <div class="header-menu-item"><a href="<?=BASE_URL?>/reserve/notice/<?=$notice['idx']?>">공지</a></div>
-              <?=!empty($notice['weather']) ? '<div class="header-menu-item"><a target="_blank" href="' . $notice['weather'] . '">날씨</a></div>' : ''?>
+              <div class="header-menu-item col-6 active"><a href="<?=BASE_URL?>/list/<?=$notice['idx']?>">좌석</a></div>
+              <div class="header-menu-item col-6"><a href="<?=BASE_URL?>/reserve/notice/<?=$notice['idx']?>">공지</a></div>
+              <?=!empty($notice['weather']) ? '<div class="header-menu-item col-6"><a target="_blank" href="' . $notice['weather'] . '">날씨</a></div>' : ''?>
               <?=!empty($notice['cctv']) ? '<div class="header-menu-item col-6"><a class="btn-video" data-source="' . $notice['cctv'] . '">영상</a></div>' : ''?>
             </div>
 
