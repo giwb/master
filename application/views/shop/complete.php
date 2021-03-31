c1e67c6e5b1b8f6005f0f839dd07d203c5227281
unknown <cbj1028@gmail.com>
2021-04-01 02:08:51 +0900

    20210329 - New Tripkorea40

--- application/views/shop/complete.php
+++ application/views/shop/complete.php
@@ -71,7 +71,7 @@
             </div>
             <div class="row align-items-center border-bottom pt-2 pb-2 pl-3 pr-3">
               <div class="col-4 col-md-3 font-weight-bold">은행 계좌</div>
-              <div class="col-8 col-md-9 p-0 pr-2">국민은행 / 288001-04-154630<br>경인웰빙산악회 (김영미)</div>
+              <div class="col-8 col-md-9 p-0 pr-2">국민은행 / 658101-01-783256 / 최병준(경인웰빙투어)</div>
             </div>
             <?php else: ?>
             <div class="border-bottom pt-2 pb-2 pl-3 pr-3 text-center">
