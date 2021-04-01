e9da8f1887c16cabb1371e424371a59cc4bd6f0c
unknown <cbj1028@gmail.com>
2021-04-01 16:41:10 +0900

    20210401 - New Tripkorea11

--- application/views/place/index.php
+++ application/views/place/index.php
@@ -15,10 +15,12 @@
               <?php if (!empty($viewType) && $viewType == 'photo'): ?>
               <div class="col-md-6 my-3">
                 <div class="card">
+                  <?php if (!empty($value['thumbnail'])): ?>
                   <div class="view overlay">
-                    <img src="<?=!empty($value['thumbnail']) ? PHOTO_PLACE_URL . 'thumb_' . $value['thumbnail'] : '/public/images/noimage.png'?>" class="card-img-top">
+                    <img src="<?=PHOTO_PLACE_URL?>thumb_<?=$value['thumbnail']?>" class="card-img-top">
                     <a href="/place/view/<?=$value['idx']?>"><div class="mask rgba-white-slight"></div></a>
                   </div>
+                  <?php endif; ?>
                   <div class="card-body">
                     <h4 class="card-title"><strong><a href="/place/view/<?=$value['idx']?>"><i class="fas fa-crown pr-1" style="color:#CE7430;"></i><?=$value['title']?><br><small class="grey-text">강원 춘천시, 홍천군<?=!empty($value['altitude']) ? ' / ' . number_format($value['altitude']) . 'm' : ''?></small></a></strong></h4><hr>
                     <p class="card-text text-justify"><?=articleContent($value['content'])?></p>
@@ -34,15 +36,15 @@
               </div>
               <?php else: ?>
               <div class="col-md-12 my-3">
-                <div class="card">
-                  <div class="card-body row">
-                    <div class="col-sm-2">
-                      <a href="/place/view/<?=$value['idx']?>"><img src="<?=!empty($value['thumbnail']) ? PHOTO_PLACE_URL . 'thumb_' . $value['thumbnail'] : '/public/images/noimage.png'?>" class="card-img-top"></a>
-                      <div class="d-block d-sm-none mb-3"></div>
+                <div class="card area-link" data-link="/place/view/<?=$value['idx']?>">
+                  <div class="card-body row no-gutters">
+                    <div class="col-3 col-sm-3" style="background: url(<?=!empty($value['thumbnail']) ? PHOTO_PLACE_URL . 'thumb_' . $value['thumbnail'] : '/public/images/nophoto.png'?>) no-repeat center center; background-size: 125%;">
                     </div>
-                    <div class="col-sm-10">
-                      <h4 class="card-title font-weight-bold"><a href="/place/view/<?=$value['idx']?>"><?=$value['title']?><br><small class="grey-text">강원 춘천시, 홍천군<?=!empty($value['altitude']) ? ' / ' . number_format($value['altitude']) . 'm' : ''?></small></a></h4><hr>
-                      <p class="card-text text-justify"><?=articleContent($value['content'], 210)?></p>
+                    <div class="col-9 col-sm-9 pl-3">
+                      <h4 class="card-title font-weight-bold"><?=$value['title']?><br><small class="grey-text">강원 춘천시, 홍천군<?=!empty($value['altitude']) ? ' / ' . number_format($value['altitude']) . 'm' : ''?></small></h4>
+                      <div class="d-none d-sm-block"><hr>
+                        <p class="card-text text-justify"><?=articleContent($value['content'], 180)?></p>
+                      </div>
                     </div>
                   </div>
                   <div class="mdb-color lighten-3 text-center">
