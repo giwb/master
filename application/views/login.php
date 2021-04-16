78f8c8d67716c4f01c5752527fb1894098905f1c
unknown <cbj1028@gmail.com>
2021-04-16 12:01:52 +0900

    20210416 - Updates6

--- application/views/login.php
+++ application/views/login.php
@@ -2,63 +2,53 @@
 
   <main id="club">
     <div class="container-fluid club-main">
-      <div class="row mt-1 mb-5">
-        <div class="col-lg-2"></div>
-        <div class="col-lg-8">
+      <div class="area-login">
+        <h2 class="font-weight-bold text-center mt-2">로그인</h2>
+        <hr class="bg-danger mb-5">
 
-          <h2 class="mt-2 text-center font-weight-bold">로그인</h2>
-          <hr class="bg-danger mb-5">
+        <form class="loginForm" method="post">
+          <div class="row align-items-center mb-2">
+            <div class="col-sm-3 font-weight-bold">아이디</div>
+            <div class="col-sm-8"><input type="text" name="login_userid" class="form-control" value="<?=get_cookie('cookie_userid')?>"></div>
+          </div>
+          <div class="row align-items-center mb-2">
+            <div class="col-sm-3 font-weight-bold">비밀번호</div>
+            <div class="col-sm-8"><input type="password" name="login_password" class="form-control" value="<?=get_cookie('cookie_passwd')?>"></div>
+          </div>
+          <div class="row align-items-center mb-2">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-8"><label class="small"><input type="checkbox" name="save"> 아이디/비밀번호 저장</label></div>
+          </div>
+          <div class="error-message text-danger text-center"></div>
+          <div class="text-center mt-3 mb-2">
+            <input type="hidden" name="redirectUrl" value="<?=$redirect_url?>">
+            <button type="button" class="btn btn-danger btn-login pl-5 pr-5">로그인</button>
+          </div>
+          <div class="text-center mr-3 ml-3">
+            <a href="<?=BASE_URL?>/login/entry"><button type="button" class="btn btn-info pl-3 pr-3">회원가입</button></a>
+            <a href="<?=BASE_URL?>/login/forgot"><button type="button" class="btn btn-secondary pl-3 pr-3">아이디/비밀번호 찾기</button></a>
+          </div>
 
-          <form class="loginForm row" method="post">
-            <div class="col-lg-3"></div>
-            <div class="col-12 col-lg-6">
-              <div class="row align-items-center mb-2">
-                <div class="col-sm-3 font-weight-bold">아이디</div>
-                <div class="col-sm-8"><input type="text" name="login_userid" class="form-control" value="<?=get_cookie('cookie_userid')?>"></div>
+          <div class="area-ad">
+            <!-- 애드핏 -->
+            <section class="section mt-4 mb-4">
+              <div class="card">
+                <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-nZvV4BAxj0QXpJeQ" data-ad-width="320" data-ad-height="100"></ins>
+                <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
               </div>
-              <div class="row align-items-center mb-2">
-                <div class="col-sm-3 font-weight-bold">비밀번호</div>
-                <div class="col-sm-8"><input type="password" name="login_password" class="form-control" value="<?=get_cookie('cookie_passwd')?>"></div>
-              </div>
-              <div class="row align-items-center mb-2">
-                <div class="col-sm-3"></div>
-                <div class="col-sm-8"><label class="small"><input type="checkbox" name="save"> 아이디/비밀번호 저장</label></div>
-              </div>
-              <div class="error-message text-danger text-center"></div>
-              <div class="text-center mt-3 mb-2">
-                <input type="hidden" name="redirectUrl" value="<?=$redirect_url?>">
-                <button type="button" class="btn btn-danger btn-login pl-5 pr-5">로그인</button>
-              </div>
-              <div class="text-center mr-3 ml-3">
-                <a href="<?=BASE_URL?>/login/entry"><button type="button" class="btn btn-info pl-3 pr-3">회원가입</button></a>
-                <a href="<?=BASE_URL?>/login/forgot"><button type="button" class="btn btn-secondary pl-3 pr-3">아이디/비밀번호 찾기</button></a>
-              </div>
+            </section>
 
-              <div class="area-ad">
-                <!-- 애드핏 -->
-                <section class="section mt-4 mb-4">
-                  <div class="card">
-                    <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-nZvV4BAxj0QXpJeQ" data-ad-width="320" data-ad-height="100"></ins>
-                    <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
-                  </div>
-                </section>
-
-                <?php if (ENVIRONMENT == 'production'): ?>
-                <!-- 구글 광고 -->
-                <section class="section">
-                  <div class="card text-center">
-                    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2424708381875991" data-ad-slot="1285643193" data-ad-format="auto" data-full-width-responsive="true"></ins>
-                    <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
-                  </div>
-                </section>
-                <?php endif; ?>
+            <?php if (ENVIRONMENT == 'production'): ?>
+            <!-- 구글 광고 -->
+            <section class="section">
+              <div class="card text-center">
+                <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2424708381875991" data-ad-slot="1285643193" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
               </div>
-
-            </div>
-            <div class="col-lg-3"></div>
-          </form>
-        </div>
-        <div class="col-lg-2"></div>
+            </section>
+            <?php endif; ?>
+          </div>
+        </form>
       </div>
     </div>
   </main>
--- application/views/member/forgot.php
+++ application/views/member/forgot.php
@@ -2,112 +2,98 @@
 
   <main id="club">
     <div class="container-fluid club-main">
-      <div class="row mt-1 mb-5">
-        <div class="col-lg-2"></div>
-        <div class="col-lg-8">
+      <div class="area-login">
+        <div id="step1">
+          <h2 class="font-weight-bold text-center mt-2">회원가입 확인</h2>
+          <hr class="bg-danger mb-5">
 
-          <div class="row align-items-center mt-2 mb-2">
-            <div class="col-1 col-lg-2"></div>
-            <div class="col-lg-8">
-              <div id="step1">
-                <h2 class="mt-2 text-center">회원가입 확인</h2>
-                <hr class="bg-danger mb-5">
-
-                <form id="formCheckPhone" method="post">
-                  <div class="row no-gutters">
-                    <div class="col-4 col-sm-3 pt-2 pl-3">휴대폰 번호</div>
-                    <div class="col-8 col-sm-9">
-                      <div class="row w-100 no-gutters align-items-center">
-                        <div class="col-3 col-sm-2 mr-2 p-0"><input type="text" name="phone1" maxlength="3" class="form-control"></div>
-                        <div class="col-4 col-sm-3 mr-2 p-0"><input type="text" name="phone2" maxlength="4" class="form-control"></div>
-                        <div class="col-4 col-sm-3 p-0"><input type="text" name="phone3" maxlength="4" class="form-control"></div>
-                        <div class="col-sm-3 ml-2 p-0 d-none d-sm-block"><button type="button" class="btn btn-secondary btn-send-auth m-0 pt-2 pb-2 pl-3 pr-3">인증번호 발송</button></div>
-                        <div class="col-12 mt-2 p-0 d-block d-sm-none"><button type="button" class="btn btn-secondary btn-send-auth m-0 pt-2 pb-2 pl-5 pr-5">인증번호 발송</button></div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="row no-gutters align-items-center mt-2">
-                    <div class="col-4 col-sm-3 pl-3">인증번호 확인</div>
-                    <div class="col-8 col-sm-9">
-                      <div class="row w-100 no-gutters align-items-center">
-                        <div class="col-6 col-sm-4 p-0"><input type="text" name="auth_code" maxlength="6" class="form-control"></div>
-                        <div class="col-6 col-sm-8 p-0"><span class="ml-2 auth-time"></span></div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <hr class="bg-danger mt-5">
-                  <div class="text-center">
-                    <div class="error-message"></div>
-                    <button class="btn btn-primary btn-member-check-complete">다음으로 ></button>
-                  </div>
-                </form>
+          <form id="formCheckPhone" method="post">
+            <div class="row no-gutters">
+              <div class="col-4 col-sm-3 pt-2 pl-3">휴대폰 번호</div>
+              <div class="col-8 col-sm-9">
+                <div class="row w-100 no-gutters align-items-center">
+                  <div class="col-3 col-sm-2 mr-2 p-0"><input type="text" name="phone1" maxlength="3" class="form-control"></div>
+                  <div class="col-4 col-sm-3 mr-2 p-0"><input type="text" name="phone2" maxlength="4" class="form-control"></div>
+                  <div class="col-4 col-sm-3 p-0"><input type="text" name="phone3" maxlength="4" class="form-control"></div>
+                  <div class="col-sm-3 ml-2 p-0 d-none d-sm-block"><button type="button" class="btn btn-secondary btn-send-auth m-0 pt-2 pb-2 pl-3 pr-3">인증번호 발송</button></div>
+                  <div class="col-12 mt-2 p-0 d-block d-sm-none"><button type="button" class="btn btn-secondary btn-send-auth m-0 pt-2 pb-2 pl-5 pr-5">인증번호 발송</button></div>
+                </div>
               </div>
-              <div id="step2" style="display: none;">
-                <h2 class="mt-2 text-center">비밀번호 변경</h2>
-                <hr class="bg-danger mb-5">
-
-                <form id="formChangePassword" method="post">
-                  <div class="row no-gutters">
-                    <div class="col-5 col-sm-3 pt-2 pl-3">가입된 아이디</div>
-                    <div class="col-7 col-sm-9 pr-3">
-                      <input readonly type="text" name="userid" class="form-control">
-                    </div>
-                  </div>
-                  <div class="row no-gutters mt-2">
-                    <div class="col-5 col-sm-3 pt-2 pl-3">새로운 비밀번호</div>
-                    <div class="col-7 col-sm-9 pr-3">
-                      <input type="password" name="password" maxlength="30" class="form-control" autocomplete="new-password">
-                    </div>
-                  </div>
-                  <div class="row no-gutters mt-2">
-                    <div class="col-5 col-sm-3 pt-2 pl-3">비밀번호 확인</div>
-                    <div class="col-7 col-sm-9 pr-3">
-                      <input type="password" name="password_check" maxlength="30" class="form-control" autocomplete="new-password">
-                    </div>
-                  </div>
-
-                  <hr class="bg-danger mt-5">
-                  <div class="text-center">
-                    <div class="error-message"></div>
-                    <input type="hidden" name="phone1">
-                    <input type="hidden" name="phone2">
-                    <input type="hidden" name="phone3">
-                    <input type="hidden" name="auth_code">
-                    <button class="btn btn-primary btn-member-change-password">비밀번호 변경 완료</button>
-                  </div>
-                </form>
-              </div>
-
-              <div class="area-ad">
-                <!-- 애드핏 -->
-                <section class="section mt-4 mb-4">
-                  <div class="card">
-                    <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-nZvV4BAxj0QXpJeQ" data-ad-width="320" data-ad-height="100"></ins>
-                    <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
-                  </div>
-                </section>
-
-                <?php if (ENVIRONMENT == 'production'): ?>
-                <!-- 구글 광고 -->
-                <section class="section">
-                  <div class="card text-center">
-                    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2424708381875991" data-ad-slot="1285643193" data-ad-format="auto" data-full-width-responsive="true"></ins>
-                    <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
-                  </div>
-                </section>
-                <?php endif; ?>
-              </div>
-
             </div>
-            <div class="col-1 col-lg-2"></div>
-          </div>
+            <div class="row no-gutters align-items-center mt-2">
+              <div class="col-4 col-sm-3 pl-3">인증번호 확인</div>
+              <div class="col-8 col-sm-9">
+                <div class="row w-100 no-gutters align-items-center">
+                  <div class="col-6 col-sm-4 p-0"><input type="text" name="auth_code" maxlength="6" class="form-control"></div>
+                  <div class="col-6 col-sm-8 p-0"><span class="ml-2 auth-time"></span></div>
+                </div>
+              </div>
+            </div>
 
+            <hr class="bg-danger mt-5">
+            <div class="text-center">
+              <div class="error-message"></div>
+              <button class="btn btn-primary btn-member-check-complete">다음으로 ></button>
+            </div>
+          </form>
         </div>
-        <div class="col-lg-2"></div>
+        <div id="step2" style="display: none;">
+          <h2 class="mt-2 text-center">비밀번호 변경</h2>
+          <hr class="bg-danger mb-5">
+
+          <form id="formChangePassword" method="post">
+            <div class="row no-gutters">
+              <div class="col-5 col-sm-3 pt-2 pl-3">가입된 아이디</div>
+              <div class="col-7 col-sm-9 pr-3">
+                <input readonly type="text" name="userid" class="form-control">
+              </div>
+            </div>
+            <div class="row no-gutters mt-2">
+              <div class="col-5 col-sm-3 pt-2 pl-3">새로운 비밀번호</div>
+              <div class="col-7 col-sm-9 pr-3">
+                <input type="password" name="password" maxlength="30" class="form-control" autocomplete="new-password">
+              </div>
+            </div>
+            <div class="row no-gutters mt-2">
+              <div class="col-5 col-sm-3 pt-2 pl-3">비밀번호 확인</div>
+              <div class="col-7 col-sm-9 pr-3">
+                <input type="password" name="password_check" maxlength="30" class="form-control" autocomplete="new-password">
+              </div>
+            </div>
+
+            <hr class="bg-danger mt-5">
+            <div class="text-center">
+              <div class="error-message"></div>
+              <input type="hidden" name="phone1">
+              <input type="hidden" name="phone2">
+              <input type="hidden" name="phone3">
+              <input type="hidden" name="auth_code">
+              <button class="btn btn-primary btn-member-change-password">비밀번호 변경 완료</button>
+            </div>
+          </form>
+        </div>
+
+        <div class="area-ad">
+          <!-- 애드핏 -->
+          <section class="section mt-4 mb-4">
+            <div class="card">
+              <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-nZvV4BAxj0QXpJeQ" data-ad-width="320" data-ad-height="100"></ins>
+              <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
+            </div>
+          </section>
+
+          <?php if (ENVIRONMENT == 'production'): ?>
+          <!-- 구글 광고 -->
+          <section class="section">
+            <div class="card text-center">
+              <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2424708381875991" data-ad-slot="1285643193" data-ad-format="auto" data-full-width-responsive="true"></ins>
+              <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
+            </div>
+          </section>
+          <?php endif; ?>
+        </div>
       </div>
     </div>
-    <input type="hidden" name="clubIdx" value="<?=!empty($clubIdx) ? $clubIdx : ''?>">
   </main>
 
   <div class="modal fade" id="forgotModal" tabindex="-1" role="dialog" aria-labelledby="forgotModalLabel" aria-hidden="true">
