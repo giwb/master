ab5b663002cbb9cea70a8f9fe553c7253a4e6a37
unknown <cbj1028@gmail.com>
2021-05-31 15:49:08 +0900

    20210531 - Updates2

--- application/views/reserve/index.php
+++ application/views/reserve/index.php
@@ -101,28 +101,26 @@
               <div class="area-bus-table">
                 <table>
                   <colgroup>
-                    <col width="4%"></col>
-                    <col width="16%"></col>
-                    <col width="4%"></col>
-                    <col width="16%"></col>
-                    <col width="4%"></col>
-                    <col width="16%"></col>
-                    <col width="4%"></col>
-                    <col width="16%"></col>
-                    <col width="4%"></col>
-                    <col width="16%"></col>
+                    <col width="5%"></col>
+                    <col width="20%"></col>
+                    <col width="5%"></col>
+                    <col width="20%"></col>
+                    <col width="5%"></col>
+                    <col width="20%"></col>
+                    <col width="5%"></col>
+                    <col width="20%"></col>
                   </colgroup>
                   <thead>
                     <tr>
                       <th colspan="4" style="border-right: 0px;"><?=count($busType) >= 2 ? $bus . '호차 - ' : ''?><?=$value['bus_name']?> <?=!empty($value['bus_license']) ? '(' . $value['bus_license'] . ')' : ''?></td>
-                      <th colspan="6" style="border-left: 0px;" class="text-right">출입구 (예약 : <?=$maxRes?>명)</th>
+                      <th colspan="4" style="border-left: 0px;" class="text-right">출입구 (예약 : <?=$maxRes?>명)</th>
                     </tr>
                   </thead>
                   <tbody>
                     <?php if ($value['seat'] > 13): ?>
                     <tr>
                       <th colspan="4" style="border-right: 0px;">운전석<?=!empty($value['bus_owner']) ? ' (' . $value['bus_owner'] . ' 기사)' : ''?></th>
-                      <th colspan="6" style="border-left: 0px;" class="text-right">보조석 (<?=getBusAssist($notice['bus_assist'], $bus)?>)</th>
+                      <th colspan="4" style="border-left: 0px;" class="text-right">보조석 (<?=getBusAssist($notice['bus_assist'], $bus)?>)</th>
                     </tr>
                     <?php endif; ?>
                     <?php
